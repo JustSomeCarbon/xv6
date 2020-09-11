6c7c3aea55fe0823b1d16bbb68877b8646652c6f
one_autumnleaf <noe.garcia@student.nmt.edu>
2020-09-11 14:51:07 -0600

    working syscall

--- syscall.c
+++ syscall.c
@@ -141,7 +141,7 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
     // modified ->
-    cprintf("%s -> %d\n", curproc->name, curproc->pid);
+    //cprintf("%s -> %d\n", curproc->name, curproc->pid);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
