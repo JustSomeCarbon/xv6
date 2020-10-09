24e4d2e275a311f8c38189cf26edfd748b8185b1
one_autumnleaf <noe.garcia@student.nmt.edu>
2020-10-08 20:05:27 -0600

    finishing nice

--- nice.c
+++ nice.c
@@ -6,5 +6,15 @@
 int
 main(int argc, char* argv[])
 {
-  // things
+  int pid, nice;
+  // check that pid and nice where given
+  if (argc < 2 || argc > 2)
+    exit();
+
+  // set variables
+  pid = atoi(argv[0]);
+  nice = atoi(argv[1]);
+  // set the nice value
+  n = set_nice(pid, nice);
+  exit();
 }
