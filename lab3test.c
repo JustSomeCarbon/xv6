753948d8eeddb07f146d7fbcb3e309693e7d8b28
one_autumnleaf <noe.garcia@student.nmt.edu>
2020-10-15 19:06:00 -0600

    lab3test functional

--- lab3test.c
+++ lab3test.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[])
 {
 	int k, n, id;
 	double x = 0, z;
-
+	
 	if(argc < 2)
 		n = 1;		// default value
 	else
 		n = atoi(argv[1]);  // from command line
-
+	
 	if(n < 0 || n > 20)
 		n = 2;
-
+	
 	id = 0;
 	for(k = 0; k < n; k++){
 		id = fork();
@@ -27,10 +27,9 @@ int main(int argc, char *argv[])
 		} else{ // child
 			printf(1, "Child %d created\n", getpid());
 			for(z = 0; z < 80000000.0; z += 0.01)
-				x = x + 1;
-			break;
+				x = x + 1;		
+			break;	
 		}
 	}
-        wait();
 	exit();
 }
